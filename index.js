2c4134657fe1005e224f8b183e66330da6fc7bd8
Reino Ristissaar <reino@ristissaar.ee>
2022-01-19 22:40:26 +0200

    added trainings route, view and controller

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Imports
+/** Imports */
 require('dotenv').config();
 const express = require('express')
 const path = require('path')
@@ -11,51 +11,53 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 
-// Route Paths
+/** Route Paths */
 const schedule = require("./routes/schedule")
 const competitionInstructions = require("./routes/competition-instructions")
 const results = require("./routes/results")
+const trainings = require("./routes/trainings")
 
 
-// Static Files
+/** Static Files */
 app.use(express.static(path.join(__dirname, 'public')))
 
 
-// Timezone information with Moment to Morgan
+/**  Timezone information with Moment to Morgan */
 morgan.token('date', (req, res, tz) => {
   return moment().tz(tz).format();
 })
 
-// Standard Apache combined log output to logs folder
+/**  Standard Apache combined log output to logs folder */
 morgan.format('commonUTCplus2', ':remote-addr - :remote-user [:date[Europe/Tallinn]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"');
 app.use(morgan('commonUTCplus2', {
   stream: fs.createWriteStream(path.join(__dirname, 'logs', 'access.log'), { flags: 'a' })
 }))
 
-// Response status for development use
+/** Response status for development use */
 app.use(morgan('dev'))
 
 
 app.use(express.urlencoded({ extended: false }))
 
 
-// Set Views
+/** Set Views */
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'pug')
 // app.set('view engine', 'ejs');
 
 
-// Define routes
+/** Define routes */
 app.use("/ajakava", schedule)
 app.use("/voistlusjuhend", competitionInstructions)
 app.use("/tulemused", results)
+app.use("/treeningud", trainings)
 
 app.get('/', (req, res) => {
   res.redirect('/ajakava')
 })
 
 
-// Server listening @ PORT
+/** Server listening @ PORT */
 app.listen(PORT, () => {
   console.log(`App "rml-web" started listening @ http://localhost:${PORT}`)
 })
---
+++ routes/trainings.js
@@ -0,0 +1,18 @@
+const express = require('express')
+let router = express.Router()
+
+let newDate = new Date(Date.now());
+
+
+router.use(function (req, res, next) {
+  next()
+})
+
+router
+  .route("/")
+  .get((req, res) => {
+    res.render("trainings")
+  })
+
+
+module.exports = router;
